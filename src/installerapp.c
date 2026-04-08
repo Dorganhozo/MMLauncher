83eaa7f53581494fb9d90091ce5c081104f001f6
Dorganhozo <131984031+Dorganhozo@users.noreply.github.com>
2026-04-08 12:22:55 -0300

    Salvando o zip do jre dentro de um arquivo temporario

--- src/installerapp.c
+++ src/installerapp.c
@@ -1,6 +1,12 @@
 #include "installerapp.h"
 #include "installerappwin.h"
+#include <asm-generic/fcntl.h>
 #include <curl/curl.h>
+#include <curl/easy.h>
+#include <curl/system.h>
+#include <stdlib.h>
+#include <string.h>
+#include <zip.h>
 #include <curl/typecheck-gcc.h>
 #include <gio/gio.h>
 #include <glib.h>
@@ -21,22 +27,19 @@ struct _InstallerApp{
 G_DEFINE_TYPE(InstallerApp, installer_app, GTK_TYPE_APPLICATION);
 static void installer_app_init(InstallerApp* app){}
 
-static size_t data_handler(char* buffer, size_t itemsize, size_t n_items, void* data){
+static size_t download_handler(char* buffer, size_t itemsize, size_t n_items, void* data){
 	InstallerApp* app = data;
 	size_t bytes = itemsize * n_items;
 	curl_off_t received, length;
 	curl_easy_getinfo(app->curl, CURLINFO_SIZE_DOWNLOAD_T, &received);
 	curl_easy_getinfo(app->curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, &length);
 
-
-
 	gdouble progress = (float)received/(float)length;
+
+	fwrite(buffer, sizeof *buffer, bytes, app->download_file);
 
 	installer_set_progress_value(app->window, progress);
 	gtk_main_iteration();
-
-	fwrite(buffer, sizeof *buffer, bytes, app->download_file);
-	fflush(app->download_file);
 
 	return bytes;
 }
@@ -46,44 +49,74 @@ static size_t data_handler(char* buffer, size_t itemsize, size_t n_items, void* 
 static void start_installation(InstallerAppWindow* win, gpointer data){
 	InstallerApp* app = INSTALLER_APP(gtk_window_get_application(GTK_WINDOW(win)));
 	app->curl = curl_easy_init();
+	app->download_file = tmpfile();
 
-
-	app->download_file = fopen(P_tmpdir"/zulujre.zip", "wr");
 	curl_easy_setopt(app->curl, CURLOPT_URL, ZULU_JRE_LINK);
-	curl_easy_setopt(app->curl, CURLOPT_WRITEFUNCTION, data_handler);
+	curl_easy_setopt(app->curl, CURLOPT_WRITEFUNCTION, download_handler);
 	curl_easy_setopt(app->curl, CURLOPT_WRITEDATA, app);
 
 	installer_message(win, "Baixando o java 8..");
 	CURLcode result = curl_easy_perform(app->curl);
 
 
-
 	if(result != CURLE_OK){
-		GString* str = g_string_new(NULL);
-		g_string_printf(str, "<span foreground=\"red\">Error ao baixar o java: %s</span>", curl_easy_strerror(result));
-		installer_message(win, str->str);
+		curl_easy_cleanup(app->curl);
 		g_printerr("curl: %s\n", curl_easy_strerror(result));
-		g_string_free(str, TRUE);
 		return;
 	}
-
+	curl_easy_cleanup(app->curl);
+	rewind(app->download_file);
 
 	installer_message(win, "Extraindo..");
 
+	curl_off_t length = 0;
+	curl_easy_getinfo(app->curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD_T, &length);
+
+	int errorp;
+	zip_t* zip;
+	zip_source_t* zsource;
+	zip_error_t zerror;
+
+	zip_error_init(&zerror);
+
+	zsource = zip_source_filep_create(app->download_file, 0, length, &zerror);
+
+	if(zsource == NULL){
+		g_printerr("zsource: %s\n", zip_error_strerror(&zerror));
+		zip_error_fini(&zerror);
+		return;
+	}
+
+	zip = zip_open_from_source(zsource, ZIP_RDONLY, &zerror);
+
+	if(zip == NULL){
+		g_printerr("zip: %s\n", zip_error_strerror(&zerror));
+		zip_error_fini(&zerror);
+		return;
+	}
+
+	for(int i=0; i < zip_get_num_entries(zip, ZIP_FL_UNCHANGED); i++){
+		const char* name = zip_get_name(zip, i, ZIP_FL_UNCHANGED);
+		g_print("%s\n", name);
+	}
+
 	fclose(app->download_file);
-	curl_easy_cleanup(app->curl);
+
+	zip_close(zip);
+
 }
 
 static void cancel_installation(InstallerAppWindow* win, gpointer data){
-	InstallerApp* app = INSTALLER_APP(gtk_window_get_application(GTK_WINDOW(win)));
+	// InstallerApp* app = INSTALLER_APP(gtk_window_get_application(GTK_WINDOW(win)));
+	g_print("Cancelado\n");
 }
 
 
 // TODO tenta usar g_resources_open_stream()
 static void installer_app_activate(GApplication* app){
-	InstallerAppWindow* win;
 	INSTALLER_APP(app)->window = installer_app_window_new(INSTALLER_APP(app));
-	win = INSTALLER_APP(app)->window;
+
+	InstallerAppWindow* win = INSTALLER_APP(app)->window;
 
 	g_signal_connect(win, "on_install_launcher", G_CALLBACK(start_installation), NULL);
 	g_signal_connect(win, "on_cancel_installation", G_CALLBACK(cancel_installation), NULL);
